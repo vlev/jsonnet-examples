0ea631d3d6587f0ed292390c2baa08fd62f4e1f9
Vitaliy <vitaliy.levashin@gmail.com>
2019-12-14 17:54:51 +0200

    helpers removed

--- docker-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-docker rm jsonnet
-docker run --name jsonnet -it -v $(pwd)/src:/src bitnami/jsonnet /src/"$1".jsonnet
-
--- local-run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-jsonnet src/$1.jsonnet
